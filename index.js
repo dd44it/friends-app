5c1610c7153208d3e7500dbcc26312a08d985433
dd44it <12nelson13@gmail.com>
2022-10-13 19:32:21 +0300

    feat: add method search by name and by phone

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  const config = {}
   const friendsList = document.querySelector('.friends-list')
+  const searchByName = document.querySelector('.filter-search__name .btn')
+  const searchByPhone = document.querySelector('.filter-search__phone .btn')
+
+  const userNodFinded = '<h2>User not finded</h2>'
   function getData(url, method = "GET"){
     const data = fetch(url, {
       method: method
     })
     const res = data.then(result => result.json())
     res.then(data => {
+      if(data.results) config.daraList = data.results
       data.results.forEach(element => {
         const user = new FriendsList(`${element.name.first} ${element.name.last}`, element.registered.age, element.phone, element.gender, element.picture.large)
         friendsList.insertAdjacentHTML('beforeend', user.render())
@@ -44,4 +50,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
   }
   getData('https://randomuser.me/api/?page=3&results=20&seed=abc')
+  searchByName.addEventListener('click', (e) => { searchData('.search_name', e.target.dataset.filter) })
+  searchByPhone.addEventListener('click', (e) => { searchData('.search_phone', e.target.dataset.filter) })
+
+  // search value
+
+  function searchData(searchSelector, findByData){
+    const searchElem = document.querySelector(searchSelector)
+    if(config.daraList){
+      config.daraList.forEach(element => {
+        if(findByData === 'name' && searchElem.value.length && `${element.name.first} ${element.name.last}`.indexOf(searchElem.value) !== -1){
+          friendsList.innerHTML = ''
+          const user = new FriendsList(`${element.name.first} ${element.name.last}`, element.registered.age, element.phone, element.gender, element.picture.large)
+          friendsList.insertAdjacentHTML('beforeend', user.render())
+          // console.log(element.name.first, element.name.last)
+        }
+        else if(findByData === 'phone' && searchElem.value.length && element.phone.indexOf(searchElem.value) !== -1){
+          friendsList.innerHTML = ''
+          const user = new FriendsList(`${element.name.first} ${element.name.last}`, element.registered.age, element.phone, element.gender, element.picture.large)
+          friendsList.insertAdjacentHTML('beforeend', user.render())
+        }
+      })
+    }
+  }
+
+  // sort type sort
+
+  // select by gender
+
+  // reset
 })
